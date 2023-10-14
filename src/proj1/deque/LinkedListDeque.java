1e2560a6f7b93be1e0b573a469af1ad5e01e2268
Tanany365 <ahmdgeek@gmail.com>
2023-10-14 19:10:47 +0300

    project 1/ build LinkedListDeque skeleton

---
+++ src/proj1/deque/LinkedListDeque.java
@@ -0,0 +1,43 @@
+package proj1.deque;
+
+import java.util.Iterator;
+
+public class LinkedListDeque<T> {
+    public LinkedListDeque() {
+
+    }
+    public T getRecursive(int index) {
+        return null;
+    }
+    public void addFirst(T item) {
+    }
+
+    public void addLast(T item) {
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return -5;
+    }
+
+    public void printDeque() {
+    }
+    public T removeFirst() {
+        return null;
+    }
+    public T removeLast() {
+        return null;
+    }
+    public T get(int index) {
+        return null;
+    }
+    public Iterator<T> iterator() {
+        return null;
+    }
+    public boolean equals(Object o) {
+        return false;
+    }
+}
