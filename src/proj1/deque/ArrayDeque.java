482960d9b73802ba10c422b74856cbad6b01808c
Tanany365 <ahmdgeek@gmail.com>
2023-11-24 19:02:52 +0200

    project 1 ArrayDeque/ setup public api skeleton

---
+++ src/proj1/deque/ArrayDeque.java
@@ -0,0 +1,52 @@
+package proj1.deque;
+
+import java.util.Iterator;
+
+public class ArrayDeque<T> {
+    private int size;
+    public ArrayDeque(T x) {
+
+        size = 1;
+    }
+
+    public ArrayDeque() {
+
+    }
+
+    public T getRecursive(int index) {
+        return null;
+    }
+    public void addFirst(T item) {
+
+        size += 1;
+    }
+
+    public void addLast(T item) {
+
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        return size < 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+    }
+    public T removeFirst() {
+        return null;
+    }
+    public T removeLast() {
+        return null;
+    }
+    public T get(int index) {
+        return null;
+    }
+
+    public boolean equals(Object o) {
+        return false;
+    }
+}
