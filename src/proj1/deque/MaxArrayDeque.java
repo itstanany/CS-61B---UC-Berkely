0af82ae0b484770b01e060a426164d38563b16f5
Tanany365 <ahmdgeek@gmail.com>
2023-12-01 20:42:30 +0200

    project 1 MaxArrayDeque / implement max method

--- src/proj1/deque/MaxArrayDeque.java
+++ src/proj1/deque/MaxArrayDeque.java
@@ -3,12 +3,27 @@ package proj1.deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends  ArrayDeque<T> {
-    public MaxArrayDeque(Comparator<T> c) {}
+    int deqArrSize = size();
+    private final Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> c) {
+        comparator = c;
+    }
     public T max() {
-        return null;
+        return max(comparator);
     }
 
     public T max(Comparator<T> c) {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        int maxDex = 0;
+        for (int i = 1; i < deqArrSize; i=+1) {
+            if (c.compare(get(i), get(maxDex)) > 0) {
+                maxDex = i;
+            }
+        }
+        return get(maxDex);
     }
+
 }
